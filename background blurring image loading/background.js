e258d94f5e988934da1638a2e98ba788f7f91eb0
Abimbola Ikuerowo <ikuerowo13@gmail.com>
2023-01-11 13:55:47 -0500

    Added Parallax and background

---
+++ background blurring image loading/background.js
@@ -0,0 +1,16 @@
+var btn1 = document.getElementById('btn-1');
+var btn2 = document.getElementById('btn-2');
+var btn3 = document.getElementById('btn-3');
+var btn4 = document.getElementById('btn-4');
+btn1.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('file:///C:/Users/DAMILARE/Desktop/html%20,%20css%20javascript%20practice/jjj/CSS%20practice/Advance/background%20blurring%20image%20loading/240718744_1058406364724184_7291793456002346695_n~2.jpg')"
+});
+btn2.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('file:///C:/Users/DAMILARE/Desktop/html%20,%20css%20javascript%20practice/jjj/CSS%20practice/Advance/background%20blurring%20image%20loading/240453322_1331565090592477_7513001117517885136_n~2.jpg')"
+});
+btn3.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('file:///C:/Users/DAMILARE/Desktop/html%20,%20css%20javascript%20practice/jjj/CSS%20practice/Advance/background%20blurring%20image%20loading/240724351_537943510767668_3004549943088210738_n~2.jpg')"
+});
+btn4.addEventListener('click', () =>{
+    document.body.style.backgroundImage = "url('file:///C:/Users/DAMILARE/Desktop/html%20,%20css%20javascript%20practice/jjj/CSS%20practice/Advance/background%20blurring%20image%20loading/240415343_188684576662296_5994877202432938014_n~2.jpg')"
+});
